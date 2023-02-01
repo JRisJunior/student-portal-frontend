b627798b3b310daa3990d114ed2fa8a580ba1250
Makayla <makaylabusher@gmail.com>
2023-02-01 12:30:22 -0600

    BrowserRouter + hardcode student id to call on show route

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,20 +1,22 @@
 import { StudentShow } from "./StudentShow";
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 export function Content() {
   const [currentStudent, setCurrentStudent] = useState({});
 
-  
 
   const handleStudentShow = (student) => {
     console.log("handleStudentShow", student);
     setCurrentStudent(student);
   }
 
-
   return (
     <div>
-      <StudentShow student={currentStudent} />
+      <Routes>
+        <Route path="/" element={<StudentShow student={currentStudent} />} />
+        <Route path="/students/:id" element={<StudentShow />} />
+      </Routes>
     </div>
   );
 }
--- src/StudentShow.jsx
+++ src/StudentShow.jsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 export function StudentShow(props) {
+  const [student, setStudent] = useState({});
+  const params = useParams();
+
+  const getStudent = () => {
+    console.log(student.id);
+    axios.get(`http://localhost:3000/students/${student.id}.json`).then(response => {
+      console.log(response.data);
+      setStudent(response.data);
+    });
+  };
+
+  useEffect(getStudent, []);
+
   return (
     <div>
       <h2>Student Information</h2>
       <h3>About</h3>
-      <p>Name: {}</p>
-      <p>Email: {}</p>
-      <p>Phone Number: {}</p>
-      <p>Bio: {}</p>
-      <p>LinkedIn: {}</p>
-      <p>Twitter Handle: {}</p>
-      <p>Personal Website: {}</p>
-      <p>Online Resume: {}</p>
-      <p>Github: {}</p>
-      <p>Photo: {}</p>
+      <p>Name: {student.first_name + " " + student.last_name}</p>
+      <p>Email: {student.email}</p>
+      <p>Phone Number: {student.phone}</p>
+      <p>Bio: {student.biography}</p>
+      <p>LinkedIn: {student.linkedin_url}</p>
+      <p>Twitter Handle: {student.twitter_user}</p>
+      <p>Personal Website: {student.website_url}</p>
+      <p>Online Resume: {student.resume_url}</p>
+      <p>Github: {student.github_url}</p>
+      <p>Photo: {student.photo_url}</p>
       <hr />
       <h3>Experience</h3>
       <p>Job Title: {}</p>
